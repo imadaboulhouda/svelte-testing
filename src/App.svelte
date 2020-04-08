086641c10cd78ff8334dc536ae4c7bce0e3c4a6c
imad aboulhouda <imadaboulhouda1@gmail.com>
2020-04-08 13:19:22 +0100

    All

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Git from './github.svelte';
+	import Git2 from './github.svelte';
+</script>
+
+<main>
+<p>Without Single value</p>
+	<Git />
+	<hr/>
+	<p>SIngle value</p>
+		<Git2 single="1"/> 
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/github.svelte
@@ -0,0 +1,24 @@
+<script>
+export let single;
+import axios from 'axios';
+let data = [];
+let url = 'https://api.github.com/users';
+if(!isNaN(single))
+{
+    url+="/"+single;
+}
+axios.get(url).then(e=>{
+    data = data.concat(e.data);
+});
+</script>
+
+<ul>
+{#each data as d}
+    <li>{d.login}- <br/>For data : {JSON.stringify(d)}</li>
+{/each}
+
+</ul>
+
+
+<style>
+</style>
